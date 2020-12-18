0d774110bb51dd9ec13446844afe785b35e75b64
Galina <krasnikova.82@mail.ru>
2020-12-18 20:53:32 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+
+//2. Реализовать класс в конструкторе которого будет счетчик количества создаваемых объектов.
+// Написать метод для получения информации о количестве.
+
+class Construktor {
+    int a;
+    int b;
+    static int NumberOfObjects =0;
+
+    Construktor (){}
+
+
+public int GetNumberOfObjects() {
+        ;
+        }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
